9425ecf45dc696b3d32dbc2cebf94b78f60a46ca
Sean <seanpai96@gmail.com>
2019-03-02 11:20:29 +0800

    shrink size

---
+++ DinnerSystem/OK/sutra.swift
@@ -0,0 +1,40 @@
+//
+//  satura.swift
+//  DinnerSystem
+//
+//  Created by Sean on 2019/3/2.
+//  Copyright © 2019 Sean.Inc. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+ 般若波羅蜜多心經
+ 
+ 
+ 觀自在菩薩，行深般若波羅密多時，照見五蘊皆空，度一切苦厄。
+ 
+ 舍利子！色不異空，空不異色；色即是空，空即是色。受想行識，亦復如是。
+ 
+ 舍利子！是諸法空相——不生不滅、不垢不淨、不增不減。是故，空中無色，無受想行識、無眼耳鼻舌身意、無色聲香味觸法、
+ 
+ 無眼界乃至無意識界、無無明亦無無明盡，乃至無老死亦無老死盡、無苦集滅道、無智亦無得。以無所得故，菩提薩埵依般若波羅密多故，心無罣礙；無罣礙故，無有恐怖，遠離顛倒夢想，究竟涅槃。三世諸佛依般若波羅密多故，得阿耨多羅三藐三菩提。
+ 
+ 故知，般若波羅密多，是大神咒、是大明咒、是無上咒、是無等等咒，能除一切苦真實不虛，故說般若波羅密多咒。
+ 
+ 即說咒曰：揭諦揭諦，波羅揭諦，波羅僧揭諦，菩提薩婆訶。
+ 
+ 
+ 　　　　THE HEART OF PRAJNA PARAMITA SUTRA
+ 
+ 　　　　When Bodhisattva Avalokiteshvara was practicing the profound Prajna Paramita,he illuminated the Five Skandhas and saw that they are all empty,and he crossed beyond all suffering and difficulty.
+ 
+ 　　　　Shariputra,form does not differ from emptiness;emptiness does not differ from form.Form itself is emptiness;emptiness itself is form.So too are feeling,cognition,formation,and consciousness.
+ 
+ 　　　　Shariputra,all Dharmas are empty of characteristics.They are not produced,not destroyed,not defiled, not pure; and they neither increase nor diminish.Therefore, in emptiness there is no form,feeling, cognition,formation,or consciousness;no eyes,ears,nose,tongue,body,or mind;no sights,sounds,smells,tastes,objects of touch,or Dharmas;no field of the eyes up to and including no field of mind consciousness;and no ignorance or ending of ignorance, up to and including no old age and death or ending of old age and death. There is no suffering, no accumulating, no extinction, and no Way, and no understanding and no attaining,because nothing is attained.
+ 
+ 　　　　The Bodhisattva through reliance on Prajna Paramita is unimpeded in his mind.Because there is no impediment,he is not afraid,and he leaves distorted dream-thinking far behind.Ultimately Nirvana!All Buddhas of the three periods of time attain Anuttara-samyak-sambodhi through reliance on Prajna Paramita.Therefore know that Prajna Paramita is a Great Spiritual Mantra,a Great Bright Mantra,a Supreme Mantra,an Unequalled Mantra.It can remove all suffering;it is genuine and not false.That is why the Mantra of Prajna Paramita was spoken.Recite it like this:
+ 
+ 　　　　Gaté Gaté Paragaté Parasamgaté Bodhi Svaha!
+ */
